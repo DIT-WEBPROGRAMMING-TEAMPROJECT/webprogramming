045c05cc4b7de693b8a0e58d706a4315136ffc28
ubu <kansuki20@gmail.com>
2021-05-30 17:49:36 +0900

    Member Table [primary key] 추가

--- taeseon/taeseon_sql.sql
+++ taeseon/taeseon_sql.sql
@@ -1,22 +1,12 @@
-create table member (
-    id nvarchar2(20),
+create table member ( -- 회원정보
+    id nvarchar2(20) constraint PK_MEMBER_id PRIMARY KEY, -- 기본키 PK_테이블명_pk인 컬럼명
     pwd nvarchar2(20),
     name nvarchar2(10),
     email nvarchar2(30),
     address nvarchar2(50),
     admin int);
 
-alter table MEMBER add constraint PK_MEMBER_id
-primary key(id); --MEMBER 테이블의 id에 기본키
-
-create table basket ( -- 장바구니 아직 안함
-    
-);
-    
-drop table member; -- MEMBER table 삭제
-drop table productinfo; -- productinfo table 삭제
-
-create table productinfo (
+create table productinfo ( -- 상품정보 테이블
     productId NUMBER constraint PK_PRODUCTINFO_PRODUCTID PRIMARY KEY, --pk
     productCode NUMBER,
     name NVARCHAR2(30),
@@ -27,10 +17,15 @@ create table productinfo (
     stock NUMBER
 );
 
-create table board (
-    memberId nvarchar2(20),
-    title nvarchar2(60),
-    regtime nvarchar2(20),
-    hits number,
-    content nvarchar2(100)
+create table board ( -- 후기게시판
+
 );
+
+
+create table basket ( -- 장바구니 아직 안함
+    
+);
+    
+drop table member; -- MEMBER table 삭제
+drop table productinfo; -- productinfo table 삭제
+drop table board; -- 후기게시판 table 삭제
