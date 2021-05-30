612e0ead69a3ec4115c8e292b71359818f0d2e6f
ubu <kansuki20@gmail.com>
2021-05-30 19:17:05 +0900

    BOARD 테이블 생성, foreign key 지정]

--- taeseon/taeseon_sql.sql
+++ taeseon/taeseon_sql.sql
@@ -1,5 +1,5 @@
 create table member ( -- 회원정보
-    id nvarchar2(20) constraint PK_MEMBER_id PRIMARY KEY, -- 기본키 PK_테이블명_pk인 컬럼명
+    id nvarchar2(20) constraint PK_MEMBER_ID PRIMARY KEY, -- 기본키 PK_테이블명_pk인 컬럼명
     pwd nvarchar2(20),
     name nvarchar2(10),
     email nvarchar2(30),
@@ -18,9 +18,23 @@ create table productinfo ( -- 상품정보 테이블
 );
 
 create table board ( -- 후기게시판
-
+    memberId NVARCHAR2(20),
+    productId NUMBER,
+    boardID NUMBER constraint PK_BOARD_BOARDID PRIMARY KEY,
+    title NVARCHAR2(60),
+    regtime date,
+    content NVARCHAR2(100),
+    -- 외래키 지정
+    constraint FK_MEMBER_BOARD_MEMBERID FOREIGN KEY(memberId)
+        REFERENCES MEMBER(id) ON DELETE CASCADE,
+    constraint FK_PRODUCTINFO_BOARD_PRODUCTID FOREIGN KEY(productID)
+        REFERENCES PRODUCTINFO(productID) ON DELETE CASCADE
 );
-
+    --외래키
+    -- CONSTRAINT [제약조건 명] FOREIGN KEY([컬럼명])
+    -- REFERENCES [참조할 테이블 이름]([참조할 컬럼])
+    -- [ON DELETE CASCADE | ON DELETE SET NULL]
+    -- 왼쪽은 부모 테이블에 값이 삭제되면 자식 테이블의 컬럼 삭제, 오른쪽은 부모 테이블에 값이 삭제되면 자식 NULL값으로 변경
 
 create table basket ( -- 장바구니 아직 안함
     
