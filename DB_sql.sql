0a7ba17d4d2b77a20fe3a3f44008eff203b1a082
kansuki20 <74278396+kansuki20@users.noreply.github.com>
2021-06-19 17:20:18 +0900

    06/19 17:19 - sql수정본

--- DB_sql.sql
+++ DB_sql.sql
@@ -1,3 +1,4 @@
+-------------------------------------- 필수 ------------------------------------------------
 create table member ( -- 회원정보
     id nvarchar2(20) constraint PK_MEMBER_ID PRIMARY KEY, -- 기본키 PK_테이블명_pk인 컬럼명
     pwd nvarchar2(20) NOT NULL,
@@ -5,11 +6,6 @@ create table member ( -- 회원정보
     email nvarchar2(30) NOT NULL,
     address nvarchar2(50) NOT NULL,
     admin int);
-select * from board where productid = 1;
-select p.name, p.thumbnaillink, b.memberId, b.content, b.regtime from board b join productinfo p
-on b.productid = p.productid
-where b.productid in (select thumbnaillink from productinfo
-                      where productid = 1);
 
 create table productinfo ( -- 상품정보 테이블
     productId NUMBER constraint PK_PRODUCTINFO_PRODUCTID PRIMARY KEY, --pk
@@ -37,9 +33,21 @@ create table board ( -- 후기게시판
 create table basket(
     memberId NVARCHAR2(20),
     productId NUMBER,
+    regtime date,
+    quantity number,
     constraint FK_MEMBER_BASKET_MEMBERID FOREIGN KEY (memberId)
         REFERENCES MEMBER(id) ON DELETE CASCADE,
     constraint FK_PRODUCTINFO_BASKET_PRODUCTID FOREIGN KEY (productID)
+        REFERENCES PRODUCTINFO(productID) ON DELETE CASCADE
+);
+create table orderlist (
+    memberId nvarchar2(20),
+    productId number,
+    quantity number,
+    regtime date,
+    constraint FK_MEMBER_ORDERLIST_MEMBERID FOREIGN KEY (memberId)
+        REFERENCES MEMBER(id) ON DELETE CASCADE,
+    constraint FK_PRODUCTINFO_ORDERLIST_PRODUCTID FOREIGN KEY (productID)
         REFERENCES PRODUCTINFO(productID) ON DELETE CASCADE
 );
     --외래키
@@ -48,7 +56,11 @@ create table basket(
     -- [ON DELETE CASCADE | ON DELETE SET NULL]
     -- 왼쪽은 부모 테이블에 값이 삭제되면 자식 테이블의 컬럼 삭제, 오른쪽은 부모 테이블에 값이 삭제되면 자식 NULL값으로 변경
 
-create sequence board_seq_auto start with 1 increment by 1 minvalue 1 maxvalue 99999999;
+create sequence board_seq_auto start with 1 increment by 1 minvalue 1 maxvalue 99999999; -- board 시퀀스
+-- productInfo 시퀀스는 나중에 관리자 기능 다 만들어졌을 때 적용
+
+-------------------------------------- 필수 ------------------------------------------------
+
 -- 시퀸스 초기화하고 싶을 때
 alter sequence board_seq_auto increment by 1;
 -- (drop으로 삭제)
@@ -66,20 +78,44 @@ select LAST_NUMBER from USER_SEQUENCES where SEQUENCE_NAME = 'board_seq_auto';
 alter sequence board_seq_auto 
 alter sequence board_seq_auto increment by 1;
 
-create table basket ( -- 장바구니 아직 안함
-    
-);
     -- DetailReview
     select * from (select rownum num, L.* from (select B.memberId, B.regtime, B.content, P.name, P.thumbnaillink from board B join productinfo P on B.productId = P.productId order by regtime desc) L) where num between 1 and 9;
     
-    select B.boardId, B.memberId, B.regtime, B.content, P.info, P.detailedlink
-    from board B join productinfo P
-    on B.productId = P.productId
-    order by regtime desc;
+    select p.name, p.thumbnaillink, p.price, count(b.memberId) as "count" from productInfo p join basket b
+    on p.productId = b.productId
+    where b.memberId = 'as'
+    group by p.name, p.thumbnaillink, p.price;
     
+    select p.name, p.thumbnaillink, b.memberId, b.regtime, b.content
+    from productInfo p join board b
+    on p.productId = b.productId
+    order by b.regtime desc;
+    select * from Board where productId = 1 order by regtime desc;
+    
+    select p.name, p.thumbnaillink, b.memberId, b.regtime, b.content from productInfo p join board b on p.productId = b.productId order by b.regtime desc
+    
+    select * from productInfo
+    where productCode = 1
+    order by productId desc;
+    
+    select p.name, p.thumbnaillink, p.price, count(b.memberId) from productInfo p join basket b on p.productId = b.productId where b.memberId = 'as' group by p.name, p.thumbnaillink, p.price;
+    
+    where productId = ;
+    (select productId, count(*) from basket
+    group by memberId, productId);
+    
+    select b.productId from basket b
+    where memberId = 'as';
+    
+    select productId, productcode, name, detailedlink, thumbnaillink, price, stock from productInfo
+    where productId in (select productId from basket
+                        where memberId = (select memberId from member
+                                          where id = 'as'));
+    select     
 drop table member; -- MEMBER table 삭제
 drop table productinfo; -- productinfo table 삭제
 drop table board; -- 후기게시판 table 삭제
+drop table basket;
 
 -- BOARD 테이블 Test용    
 select to_char(regtime, 'yy/mm/dd hh24:mi') from board;
